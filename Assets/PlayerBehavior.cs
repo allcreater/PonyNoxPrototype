09b42a10206906e822cfa82a31691832a2d905d7
Semenov Anton <allcreater@gmail.com>
2015-02-25 22:38:19 +0300

    first commit

---
+++ Assets/PlayerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerBehavior : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private bool grounded = false;
+
+	void Start ()
+    {
+	
+	}
+	
+    void FixedUpdate()
+    {
+        if (grounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection += (Physics.gravity * Time.deltaTime);
+        
+        var characterController = GetComponent<CharacterController>();
+        CollisionFlags flags = characterController.Move(moveDirection * Time.deltaTime);
+        grounded = (flags & CollisionFlags.CollidedBelow) != 0;
+    }
+}
