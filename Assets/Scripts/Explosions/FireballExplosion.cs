3b0c61c267aa8942aab0c7a8c151cd870ce6495a
Semenov Anton <allcreater@gmail.com>
2015-04-15 03:13:30 +0300

    Взрывы вынесены в отдельный компонент
    Чтоб бомбануло так бомбануло!

---
+++ Assets/Scripts/Explosions/FireballExplosion.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireballExplosion : MonoBehaviour
+{
+    public float m_ExplosionRadius = 5.0f;
+    public float m_ExplosionBaseDamage = 10.0f;
+    public float m_ShockwaveBaseImpulse = 100.0f;
+
+    void Explode(Vector3 location)
+    {
+        //взрываемся
+        var objects = Physics.OverlapSphere(transform.position, m_ExplosionRadius);
+        foreach (var collider in objects)
+        {
+            /*
+            if (collider.isTrigger)
+                continue; //триггеры нематериальны, нас они не пока интересуют
+            */
+            var direction = collider.transform.position - transform.position;
+            if (direction.magnitude == 0.0f) Debug.LogError("WTF?!");
+
+            float influence = Mathf.Clamp(1.0f - direction.magnitude / m_ExplosionRadius, 0, 1);
+
+            var rb = collider.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                float impulse = m_ShockwaveBaseImpulse * influence;
+                rb.AddExplosionForce(impulse, transform.position, m_ExplosionRadius);
+            }
+
+            var lc = collider.GetComponent<LivingCreatureBehaviour>();
+            if (lc != null)
+            {
+                lc.m_HitPoints.ChangeValue(-m_ExplosionBaseDamage * influence);
+            }
+        }
+    }
+}
--- Assets/Scripts/FireballBehaviour.cs
+++ Assets/Scripts/FireballBehaviour.cs
@@ -13,9 +13,6 @@ public class ThrowableSpellBehaviour : MonoBehaviour
     }
 
     public float m_ColliderRadius = 0.1f;
-    public float m_ExplosionRadius = 5.0f;
-    public float m_ExplosionBaseDamage = 10.0f;
-    public float m_ShockwaveBaseImpulse = 100.0f;
 
     public GameObject m_explosionEffect;
 
@@ -23,40 +20,15 @@ public class ThrowableSpellBehaviour : MonoBehaviour
 
     protected void Explode (Vector3 position)
     {
-        //взрываемся
-        var objects = Physics.OverlapSphere(transform.position, m_ExplosionRadius);
-        foreach (var collider in objects)
-        {
-            /*
-            if (collider.isTrigger)
-                continue; //триггеры нематериальны, нас они не пока интересуют
-            */
-            var direction = collider.transform.position - transform.position;
-            if (direction.magnitude == 0.0f) Debug.LogError("WTF?!");
-
-            float influence = Mathf.Clamp(1.0f - direction.magnitude / m_ExplosionRadius, 0, 1);
-
-            var rb = collider.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                float impulse = m_ShockwaveBaseImpulse * influence;
-                rb.AddExplosionForce(impulse, transform.position, m_ExplosionRadius);
-            }
-
-            var lc = collider.GetComponent<LivingCreatureBehaviour>();
-            if (lc != null)
-            {
-                lc.m_HitPoints.ChangeValue(-m_ExplosionBaseDamage * influence);
-            }
-        }
-
         //самоуничтожаемся
         GameObject.DestroyObject(gameObject, 2.0f);
         enabled = false;
 
         if (m_explosionEffect != null)
         {
-            var obj = GameObject.Instantiate(m_explosionEffect, position, transform.rotation);
+            var obj = GameObject.Instantiate(m_explosionEffect, position, transform.rotation) as GameObject;
+            obj.SendMessage("Explode", position);
+
             GameObject.Destroy(obj, 5.0f);
         }
     }
