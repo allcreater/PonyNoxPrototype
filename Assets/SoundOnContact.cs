9f2de66645363bcaff94abf4f1f2ca238331d298
Ke11y <kellerdom@ya.ru>
2015-05-30 02:05:16 +0400

    Звуки копыт, переключение погоды
    Ахтунг! Переключение сделано криво!

---
+++ Assets/SoundOnContact.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOnContact : MonoBehaviour
+{
+	private AudioSource m_as;
+
+	private Vector3 m_lastPosition;
+	private Vector3 m_acceleration;
+
+	void Start ()
+	{
+		m_as = GetComponent<AudioSource> ();
+		m_lastPosition = transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		var position = transform.position;
+		m_acceleration = (m_lastPosition - position) / Time.fixedDeltaTime;
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (true) {
+			Debug.Log (m_acceleration.magnitude);
+			m_as.Play ();
+		}
+	}
+}
