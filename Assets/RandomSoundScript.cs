cd7167c9c1f4212cdc27da10d1d5761223413350
Ke11y <kellerdom@ya.ru>
2015-05-31 20:24:08 +0400

    Звук в Камне

---
+++ Assets/RandomSoundScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSoundScript : MonoBehaviour {
+	public float m_MinInterval;
+	public float m_MaxInterval;
+	private AudioSource m_as;
+	// Use this for initialization
+	void Start () {
+		m_as = GetComponent<AudioSource> ();
+		StartCoroutine  (PlaySound());
+	}
+	
+	// Update is called once per frame
+	IEnumerator PlaySound () {
+		m_as.Play ();
+		yield return new WaitForSeconds (Random.Range (m_MinInterval, m_MaxInterval));
+	}
+}
